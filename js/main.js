bc5ef8d2bbb300b8b073d82b44289d6ea9195e91
Matthew Kolodziejczak <makolodzi@gmail.com>
2016-06-09 14:50:20 -0400

    Check for 3 in a row each time user picks a box

--- js/main.js
+++ js/main.js
@@ -7,17 +7,19 @@ var model = {
 		ai: null
 	},
 
-	boxes: {
-		topLeft: null,
-		topMiddle: null,
-		topRight: null,
-		middleLeft: null,
-		middleMiddle: null,
-		middleRight: null,
-		bottomLeft: null,
-		bottomMiddle: null,
-		bottomRight: null,
-	}
+	board: {
+		topLeft: "",
+		topMiddle: "",
+		topRight: "",
+		middleLeft: "",
+		middleMiddle: "",
+		middleRight: "",
+		bottomLeft: "",
+		bottomMiddle: "",
+		bottomRight: "",
+	},
+
+	winner: false
 
 };
 
@@ -36,12 +38,12 @@ var controller = {
 		model.avatar.ai = ai;
 	},
 
-	updateBox: function(boxName){
-		model.boxes[boxName] = "boom";
+	updateBox: function(box, userOrAI){
+		model.board[box] = controller.getAvatar(userOrAI);
 	},
 
-	getBox: function(boxName){
-		return model.boxes[boxName];
+	getBox: function(box){
+		return model.board[box];
 	},
 
 	getAvatar: function(userOrAI){
@@ -49,6 +51,73 @@ var controller = {
 			return model.avatar.user;
 		} else if(userOrAI === 'ai'){
 			return model.avatar.user;
+		}
+	},
+
+	isWinner: function(){
+		return model.winner;
+	},
+
+	updateWinner: function(boolean){
+		model.winner = boolean;
+	},
+
+	getBoard: function(){
+		return model.board;
+	},
+
+	checkForWinner: function(){
+		var board = controller.getBoard();
+		board = [
+			[  board.topLeft,     board.topMiddle,     board.topRight     ],
+			[  board.middleLeft,  board.middleMiddle,  board.middleRight  ],
+			[  board.bottomLeft,  board.bottomMiddle,  board.bottomRight  ]
+		];
+
+		var check = "X";
+
+		// value should be false
+		var winner = controller.isWinner();
+
+		// Check all horizontal rows for 3 matches
+		for(var i = 0; i < board.length; i++){
+			for (var b = 0; b < board[i].length; b++){
+				if(board[i][b] !== check){
+					winner = false;
+					break;
+				}
+				winner = true;
+			}
+			if(winner === true){
+				controller.updateWinner(true);
+				return;
+			}
+		}
+
+		// Check vertical columns for 3 matches
+		for(var j = 0; j < board.length; j++){
+			for (var k = 0; k < board[j].length; k++){
+				if(board[k][j] !== check){
+					winner = false;
+					break;
+				}
+				winner = true;
+			}
+			if(winner === true){
+				controller.updateWinner(true);
+				return;
+			}
+		}
+
+		// Check diagonals for 3 matches		
+		if(board[0][0] === check && board[1][1] === check && board[2][2] ===check){
+			winner = true;
+			controller.updateWinner(true);
+			return;
+		} else if(board[0][2] === check && board[1][1] === check && board[2][0] === check){
+			winner = true;
+			controller.updateWinner(true);
+			return;
 		}
 	}
 
@@ -90,11 +159,12 @@ var view = {
 		$(".box").on("click", function(){
 			var box = $(this).attr('id');
 			// if box is already filled
-			if(controller.getBox(box) !== null){
+			if(controller.getBox(box) !== ""){
 				return;
 			} else {
-				controller.updateBox(box);
+				controller.updateBox(box, 'user');
 				$(this).text(controller.getAvatar('user'));
+				controller.checkForWinner();
 			}
 		});
 	}
